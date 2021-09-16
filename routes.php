66f9c6350a59dd7b2136977a5aca4f509269743d
Octavian Popa <octavian.popa@sprintsoft.ro>
2021-09-16 13:40:41 +0200

    eroare incarcare dashbord - PFOC3-201

--- routes.php
+++ routes.php
@@ -1,15 +1,13 @@
 <?php
 
-if (config('bedard.rainlabblogapi::apiEnable')) {
-    $prefix = config('bedard.rainlabblogapi::apiPrefix');
+$prefix = 'api/v1/blog';
 
-    Route::prefix($prefix)->middleware('web')->group(function () {
-        // categories
-        Route::get('categories', 'Bedard\RainLabBlogApi\Http\Controllers\CategoriesController@index');
-        Route::get('categories/{slug}', 'Bedard\RainLabBlogApi\Http\Controllers\CategoriesController@show');
+Route::prefix($prefix)->middleware('web')->group(function () {
+    // categories
+    Route::get('categories', 'Bedard\RainLabBlogApi\Http\Controllers\CategoriesController@index');
+    Route::get('categories/{slug}', 'Bedard\RainLabBlogApi\Http\Controllers\CategoriesController@show');
 
-        // posts
-        Route::get('posts', 'Bedard\RainLabBlogApi\Http\Controllers\PostsController@index');
-        Route::get('posts/{slug}', 'Bedard\RainLabBlogApi\Http\Controllers\PostsController@show');
-    });
-}
+    // posts
+    Route::get('posts', 'Bedard\RainLabBlogApi\Http\Controllers\PostsController@index');
+    Route::get('posts/{slug}', 'Bedard\RainLabBlogApi\Http\Controllers\PostsController@show');
+});
